926f71269d34042895a25015245a87137dd3dd6c
Werner Buys <WBuys@jhb.dvt.co.za>
2019-02-11 17:00:37 +0200

    added 3 cart option items

---
+++ logic.js
@@ -0,0 +1,18 @@
+var cart = document.getElementsByClassName("cart");
+var carouselItem = document.getElementsByClassName("carousel-content-item");
+var carousel = document.getElementsByClassName("carousel");
+var content1 = document.getElementsByClassName("content1");
+var content2 = document.getElementsByClassName("content2");
+
+function displayContent() {
+    if(cart !== null) {
+        
+    }
+    else{
+
+    }
+}
+
+function closeContent() {
+
+}
