c7399c2ae81b822048443286cc57c4db1f716192
Werner Buys <WBuys@jhb.dvt.co.za>
2019-02-18 09:35:36 +0200

    added images

--- logic.js
+++ logic.js
@@ -1,25 +1,27 @@
 function DisplayAboutMe() {
     var aboutMe = document.getElementById("one").classList;
     RemoveOtherStyle();
-    aboutMe.add("display");
+    aboutMe.remove("bio");
+    aboutMe.add("display-element");
 }
 
 function DisplaySkills() {
     var skill = document.getElementById("two").classList;
     RemoveOtherStyle();
-    skill.add("display");
+    skill.add("display-element");
 }
 
 function DisplayInterest() {
     var interest = document.getElementById("three").classList;
     RemoveOtherStyle();
-    interest.add("display");
+    interest.add("display-element");
 }
 
 function DisplayWhatILove() {
     var whatILove = document.getElementById("forth").classList;
     RemoveOtherStyle();
-    whatILove.add("display");
+
+    whatILove.add("display-element");
 }
 
 function RemoveOtherStyle() {
@@ -27,21 +29,21 @@ function RemoveOtherStyle() {
     var skill = document.getElementById("two").classList;
     var interest = document.getElementById("three").classList;
     var whatILove = document.getElementById("forth").classList;
-    if(aboutMe.contains("display")) {
-        aboutMe.remove("display");
-        aboutMe.add("remove");
+    if(aboutMe.contains("display-element")) {
+        aboutMe.remove("display-element");
+        aboutMe.add("remove-element");
     }
-    else if(skill.contains("display")) {
-        skill.remove("display");
-        skill.add("remove");
+    else if(skill.contains("display-element")) {
+        skill.remove("display-element");
+        skill.add("remove-element");
     }
-    else if(interest.contains("display")) {
-        interest.remove("display");
-        interest.add("remove");
+    else if(interest.contains("display-element")) {
+        interest.remove("display-element");
+        interest.add("remove-element");
     }
-    else if(whatILove.contains("display")) {
-        whatILove.remove("display");
-        whatILove.add("remove");
+    else if(whatILove.contains("display-element")) {
+        whatILove.remove("display-element");
+        whatILove.add("remove-element");
     }
     else{
 
